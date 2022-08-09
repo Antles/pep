a674994ec629a0b66bb0077a85b1d0ad76489363
Austin Antles <antles@near.local>
2022-08-09 10:43:21 -0700

    Remove Boilerplate

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import pepReducer from "../features/pep/pepSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    pep: pepReducer,
   },
 });
